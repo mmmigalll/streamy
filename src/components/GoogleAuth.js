300a35f5b81d8e8ac5b72b9e70bcac4a1473bc08
migall <vashkebam1991@gmail.com>
2019-05-10 21:49:27 +0300

    feat(Components/GoogleAuth): raw GoogleAuth component for auth via Google Auth2

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -11,6 +11,7 @@ class GoogleAuth extends React.Component {
       }).then(() => {
         this.auth = window.gapi.auth2.getAuthInstance();
         this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+        this.auth.isSignedIn.listen(this.onAuthChange);
       }).catch((err) => {
         // TODO: handle error properly
         console.log(err);
@@ -18,13 +19,40 @@ class GoogleAuth extends React.Component {
     });
   }
 
+  onAuthChange = () => {
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  }
+
+  onSignIn = () => {
+    this.auth.signIn();
+  };
+  onSignOut = () => {
+    this.auth.signOut();
+  };
+
   renderAuthButton() {
     if (this.state.isSignedIn === null) {
-      return <div>I don`t know if we are signed in</div>
+      return null;
     } else if (this.state.isSignedIn) {
-      return <div>I am signed in</div>;
+      return (
+        <button
+          className="ui red google button"
+          onClick={this.onSignOut}
+        >
+          <i className="google icon" />
+          Sign Out
+        </button>
+      );
     } else {
-      return <div>I am not signed in</div>;
+      return (
+        <button
+          className="ui red google button"
+          onClick={this.onSignIn}
+        >
+          <i className="google icon" />
+          Sign in with Google
+        </button>
+      );
     }
   }
 
