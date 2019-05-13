bb83e0fe3d9ddadce431d057ff1c1917946c0769
migall <vashkebam1991@gmail.com>
2019-05-13 22:25:54 +0300

    feat: connect to streamy api server, refactor StreamCreate component, add StreamEdit component

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchStream, editStream } from '../../actions';
+import StreamFrom from './StreamForm';
 
-const StreamEdit = () => {
-  return (
-    <div>StreamEdit</div>
-  );
-};
+class StreamEdit extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
 
-export default StreamEdit;
+  onSubmit = (formValues) => {
+    this.props.editStream(this.props.match.params.id, formValues);
+  };
+
+  render() {
+    if (!this.props.stream) {
+      return <div>Loading...</div>
+    }
+
+    const { title, description } = this.props.stream;
+
+    return (
+      <div>
+        <h3>Edit a Stream</h3>
+        <StreamFrom
+          initialValues={{ title, description }}
+          onSubmit={this.onSubmit} />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
+}
+
+export default connect(
+  mapStateToProps,
+  { fetchStream, editStream }
+)(StreamEdit);
